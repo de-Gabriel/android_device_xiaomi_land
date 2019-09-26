979c8e1b45b39036dfb3f11ee45b3313c93377bb
AdityaKevin <adityakevin50@gmail.com>
2019-09-26 13:14:46 +0000

    land: lets make build done

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -240,6 +240,7 @@ TARGET_USES_OLD_MNC_FORMAT := true
 TARGET_RECOVERY_FSTAB 		 := $(DEVICE_PATH)/rootdir/fstab.qcom
 
 # SELinux
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-tmp
 include device/qcom/sepolicy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
