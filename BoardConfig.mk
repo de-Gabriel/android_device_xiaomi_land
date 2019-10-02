63d7d290a89506d03d70a5b535da992e1e7ecde2
AdityaKevin <adityakevin50@gmail.com>
2019-10-02 01:37:06 +0000

    land: match sepolicy pathmap

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -231,7 +231,7 @@ TARGET_USES_OLD_MNC_FORMAT := true
 TARGET_RECOVERY_FSTAB 		 := $(DEVICE_PATH)/rootdir/fstab.qcom
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/Android.mk
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 #Treble
